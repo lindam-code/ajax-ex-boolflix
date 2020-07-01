84696baabe1138f22856eb0525dbd205e6d145fe
lindam <linda.minotti2@gmail.com>
2020-07-01 12:04:11 +0200

    Creata funzione di chiamata API, associata all'evento click del bottone con input dell'User.

--- js/script.js
+++ js/script.js
@@ -1,24 +1,38 @@
 $(document).ready(function(){
   // API key: 4c34d07e5d578ee7bace09dde277dacb
   // API eg: https://api.themoviedb.org/3/movie/550?api_key=4c34d07e5d578ee7bace09dde277dacb
-  // Inizio chiamata Ajax per vedere film
-  $.ajax(
-    {
-      url: 'https://api.themoviedb.org/3/search/movie',
-      method: 'GET',
-      data: {
-        api_key: '4c34d07e5d578ee7bace09dde277dacb',
-        language: 'it-IT',
-        query: 'interrotte'
-      },
-      success: function(dataResponse){
-        console.log(dataResponse.results);
-      },
-      error: function(){
-        alert: ('Si è verificato un errore sulla chiamata API');
-      }
-    }
-  );
-  // Fine chiamata Ajax per vedere film
 
+  // Assoccio un evento click al bottone invia
+  $('#title-submit').click(function(){
+    // Creo la variabile stringa da passare alla chiamata
+    // Prendendo il valore della input
+    var titleFilm = $('#search-film').val();
+    chiamatAjax(titleFilm);
+  });
+
+  // FUNZIONI
+  // Funzione che fa la chiamata Aajax per i film
+  // Accetta: queryUser, stringa del titolo (o una parte) del film  da cercare
+  // Return:
+  function chiamatAjax(queryUser) {
+    // Inizio chiamata Ajax
+    $.ajax(
+      {
+        url: 'https://api.themoviedb.org/3/search/movie',
+        method: 'GET',
+        data: {
+          api_key: '4c34d07e5d578ee7bace09dde277dacb',
+          language: 'it-IT',
+          query: queryUser
+        },
+        success: function(dataResponse){
+          console.log(dataResponse.results);
+        },
+        error: function(){
+          alert: ('Si è verificato un errore sulla chiamata API');
+        }
+      }
+    );
+      // Fine chiamata Ajax per vedere film
+  };
 });
